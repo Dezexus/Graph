3ca2a9fab57cf1e806ff4bffd5da53448029728a
Daniil Tanygin <xdezerusx@gmail.com>
2023-03-08 16:46:13 +0300

    Исправлен метод CountingDegreesVertices; Добавлены свойства; Исправлена перегрузка сложения

--- Classes/Graph.cs
+++ Classes/Graph.cs
@@ -7,88 +7,69 @@ namespace Classes
     public class Graph
     {
 
-        #region Field
-
-        
-        private List<short> vertexColors = new List<short>();
-        private List<short> vertexDegrees = new List<short>();
-
-        #endregion
-
         public Graph(List<List<short>> _graphAsAlgebraicStructurePage) {
 
             GraphAsAlgebraicStructure = _graphAsAlgebraicStructurePage;
         }
 
-        #region Getters
+        #region Properties
 
         public List<List<short>> GraphAsAlgebraicStructure { get; set; } 
             = new List<List<short>>();
 
-        public List<short> GetVertexColors() => vertexColors;
+        public List<short> vertexColors { get; private set; } = new List<short>();
 
-        public List<short> GetVertexDegrees() => vertexDegrees;
+        public List<short> VertexDegrees { get; private set; } = new List<short>();
 
-        public int GetDegreeVertex(short vertexNumber) => vertexColors[vertexNumber];
-
-        public short GetCountVertex() => GraphAsAlgebraicStructure[0][0];
+        public short CountVertex { 
+            
+            get {
+               return GraphAsAlgebraicStructure[0][0];
+            }
+            set {
+                GraphAsAlgebraicStructure[0][0] = value;
+            }
+        }
 
         #endregion
 
         #region Methods
 
+        /// <summary>
+        /// Цвет вершины по её номеру
+        /// </summary>
+        /// <param name="vertexNumber">Номер вершины</param>
+        /// <returns>Возвращает цвет вершины</returns>
+        public int GetColorVertexByNumber(short vertexNumber) => vertexColors[vertexNumber];
+
+        /// <summary>
+        /// Создаёт массив с кол-вом степеней для каждой вершины
+        /// </summary>
         public void CountingDegreesVertices() {
 
-            //vertexDegrees = new List<short>(n);
-            for (int i = 0; i < GraphAsAlgebraicStructure[0][0]; i++)
-                vertexDegrees.Add(0);
-
-
+            for (int i = 0; i <= CountVertex; i++)
+                VertexDegrees.Add(0);
 
             for (short i = 1; i < GraphAsAlgebraicStructure.Count(); i++)
-                for (short j = 0; j < GraphAsAlgebraicStructure[0][0]; j++)
+                for (short j = 0; j <= CountVertex; j++)
                     if (GraphAsAlgebraicStructure[i][0] == j || GraphAsAlgebraicStructure[i][1] == j)
-                        vertexDegrees[j]++;
+                        VertexDegrees[j]++;
         }
 
+        /// <summary>
+        /// Выполняет поиск вершины с максимальной степенью
+        /// </summary>
+        /// <returns>Возвращает первую вершину с макс степенью</returns>
         public short SearchVertexWithMaxDegree() {
 
-            List<short> DegreeAndVertex = new List<short> { 1, 0 };
-
-            for (short i = 0; i < vertexDegrees.Count(); i++)
-                if (vertexDegrees[i] > DegreeAndVertex[0])
-                    DegreeAndVertex = new List<short> { DegreeAndVertex[0]++, i };
-
-            return DegreeAndVertex[1];
+           return (short)VertexDegrees.FindIndex(x => x == VertexDegrees.Max()); 
         }
-
-        #endregion
-
-        #region Operator
-
-        public static Graph operator +(Graph graph1, Graph graph2) {
-
-            graph1.CountingDegreesVertices();
-            graph2.CountingDegreesVertices();
-            short countVertex1 = graph1.GetCountVertex();
-            List<List<short>> matrix1 = graph1.GraphAsAlgebraicStructure;
-            List<List<short>> matrix2 = graph2.GraphAsAlgebraicStructure;
-
-            matrix1[0][0] = (short)(graph1.GraphAsAlgebraicStructure[0][0] + graph2.GraphAsAlgebraicStructure[0][0]);
-
-            for (short i = 1; i < matrix2.Count; i++) { 
-
-                short vertex1 = (short)(matrix2[i][0] + countVertex1);
-                short vertex2 = (short)(matrix2[i][1] + countVertex1);
-                matrix1.Add(new List<short>{ vertex1, vertex2});
-            }
-
-            return new Graph(matrix1);
-        }
-
-        #endregion
-
-        public List<List<short>> GenerationGraph(int countVertex) {
+        /// <summary>
+        /// Генирирует новый граф
+        /// </summary>
+        /// <param name="countVertex">Кол-во вершин графа</param>
+        /// <returns>Возращает объект типа Graph</returns>
+        public Graph GenerationGraph(int countVertex) {
 
             var rnd = new Random();
 
@@ -106,8 +87,41 @@ namespace Classes
                 graph.Add(new List<short>{ firsVertex, secondVertex });
             }
 
-	        return graph;
+	        return new Graph(graph);
         }
+        #endregion
+
+        #region Operator
+        /// <summary>
+        /// Объединяет два графа
+        /// </summary>
+        /// <param name="graph1">Граф 1</param>
+        /// <param name="graph2">Граф 2</param>
+        /// <returns>Возвращяет новый граф</returns>
+        public static Graph operator +(Graph graph1, Graph graph2) {
+
+            graph1.CountingDegreesVertices();
+            graph2.CountingDegreesVertices();
+            short vertexCount1 = graph1.CountVertex;
+            var matrix1 = new List<List<short>>(graph1.GraphAsAlgebraicStructure.);
+            var matrix2 = new List<List<short>>(graph2.GraphAsAlgebraicStructure);
+
+            matrix1[0][0] = (short)(graph1.CountVertex + graph2.CountVertex);
+
+            for (short i = 1; i < matrix2.Count; i++) { 
+
+                short vertex1 = (short)(matrix2[i][0] + vertexCount1);
+                short vertex2 = (short)(matrix2[i][1] + vertexCount1);
+                matrix1.Add(new List<short>{ vertex1, vertex2});
+            }
+            short vertex3 = graph1.SearchVertexWithMaxDegree();
+            short vertex4 = (short)(graph2.SearchVertexWithMaxDegree() + vertexCount1);
+            matrix1.Add(new List<short> {vertex3 , vertex4});
+
+            return new Graph(matrix1);
+        }
+
+        #endregion
 
     }
 }
