056d614eff9fe14e970dd6d443ad1be5cdf1534d
Daniil Tanygin <xdezerusx@gmail.com>
2023-03-08 21:55:46 +0300

    Добавлены классы сохрания настроек в xml файл

---
+++ Classes/Props.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+
+namespace XMLFileSettings
+{
+    public class PropsFields
+    {
+        public String XMLFileName = AppDomain.CurrentDomain.BaseDirectory + "settings.xml";
+
+        public int selectModeTheme = 0;
+
+    }
+
+
+    class Props {
+
+        public PropsFields Fields;
+
+        public Props() {
+
+            Fields = new PropsFields();
+        }
+
+        //Запись настроек в файл
+        public void WriteXml() {
+
+            XmlSerializer ser = new XmlSerializer(typeof(PropsFields));
+
+            TextWriter writer = new StreamWriter(Fields.XMLFileName);
+            ser.Serialize(writer, Fields);
+            writer.Close();
+        }
+
+        //Чтение насроек из файла
+        public void ReadXml() {
+
+            if (File.Exists(Fields.XMLFileName)) {
+
+                XmlSerializer ser = new XmlSerializer(typeof(PropsFields));
+                TextReader reader = new StreamReader(Fields.XMLFileName);
+                Fields = ser.Deserialize(reader) as PropsFields;
+                reader.Close();
+            }
+        }
+    }
+}
---
+++ Classes/Setting.cs
@@ -0,0 +1,26 @@
+﻿using XMLFileSettings;
+
+namespace Classes
+{
+    class Setting
+    {
+
+        public static void LoadSetting() {
+
+            Props props = new Props();
+
+            props.ReadXml();
+            Appearance.SetTheme = (Appearance.Themes)props.Fields.selectModeTheme;
+            new Appearance();
+        }
+
+        public static void SaveSetting() {
+
+            Props props = new Props();
+
+            props.Fields.selectModeTheme = (int)Appearance.SelectedTheme;
+            props.WriteXml();
+        }
+
+    }
+}
