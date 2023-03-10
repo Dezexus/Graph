daf14b2587ffd4dab0a8f5dde67c0f26a92c5dfd
Daniil Tanygin <xdezerusx@gmail.com>
2023-03-10 22:24:54 +0300

    Добавлен генератор рандомного цвета

---
+++ Classes/ColorGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Media;
+
+namespace Classes
+{
+    internal class ColorGenerator
+    {
+        private static int Count = 0; 
+
+        public static Color GetRandColor()
+        {
+            Count++;
+            Random rnd = new Random(Count);
+            string hexOutput = String.Format("{0:X}", rnd.Next(0, 0xFFFFFF));
+            while (hexOutput.Length < 6)
+                hexOutput = "0" + hexOutput;
+            return (Color)ColorConverter.ConvertFromString("#" + hexOutput);
+        }
+    }
+}
